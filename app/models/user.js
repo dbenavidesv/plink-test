fbd1bb6683e613f15453f268e8bccd266adc280c
Daniel Benavides <daniel.benavides@wolox.co>
2019-06-25 13:20:09 -0500

    Added sequelize user model

---
+++ app/models/user.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      username: { type: DataTypes.STRING, unique: true, allowNull: false },
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      password: { type: DataTypes.STRING, allowNull: false },
+      // eslint-disable-next-line new-cap
+      preferredCurrency: { type: DataTypes.ENUM(['COP', 'USD', 'EUR']) }
+    },
+    {
+      tableName: 'users',
+      underscored: true
+    }
+  );
+  return User;
+};
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,5 @@
-// const controller = require('./controllers/controller');
-const { healthCheck } = require('./controllers/healthCheck');
+const { healthCheck } = require('./controllers/health-check');
+
 exports.init = app => {
   app.get('/health', healthCheck);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
